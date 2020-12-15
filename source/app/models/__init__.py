777f77b90d5213fdd5320684b76ddb442bd709c5
depthlost <iyaelpereyra@gmail.com>
2020-12-15 17:01:06 -0300

    Se agregaron empleados docentes, no docentes y administrativos.

--- source/app/models/__init__.py
+++ source/app/models/__init__.py
@@ -6,7 +6,10 @@ from .career import Career
 from .cathedra import Cathedra
 from .office import Office
 from .charge import Charge
-from .employee import Employee
+from .employee.employee import Employee
+from .employee.docent import Docent
+from .employee.not_docent import NotDocent
+from .employee.administrative import Administrative
 from .job_position import JobPosition
 from .request import Request
 from .request_type import RequestType
