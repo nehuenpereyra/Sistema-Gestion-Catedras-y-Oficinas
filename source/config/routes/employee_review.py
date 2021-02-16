9b4777b2e11b58896f7210c2e570cd588f5bc62a
Iyael Pereyra <iyaelpereyra@gmail.com>
2021-02-16 17:14:01 -0300

    Se agrego la funcionalidad de ver una pagina completa de revisiones de empleado.

--- source/config/routes/employee_review.py
+++ source/config/routes/employee_review.py
@@ -8,3 +8,5 @@ def set_routes(app):
                      "employee_review_index", employee_review.index)
     app.add_url_rule("/employee_review/see/<int:review_id>",
                      "employee_review_see", employee_review.see)
+    app.add_url_rule("/employee_review/see_page/<int:page>",
+                     "employee_review_see_page", employee_review.see_page)
